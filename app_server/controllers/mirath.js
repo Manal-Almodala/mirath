317e6abeabf5ae295ec2910a76c6ce6f248a21b1
Yasso96 <yassin.abdohassan@gmail.com>
2017-12-21 17:17:24 +0800

    Develop controllers for all mirath's views

--- app_server/controllers/mirath.js
+++ app_server/controllers/mirath.js
@@ -11,21 +11,73 @@ var apiOptions = {
     }
 }
 
-/* GET mirath page 1 */
-module.exports.getFirstPage = function(req, res)
+module.exports = 
 {
-    renderFirstPage(req,res,)
+    getHome: function(req, res)
+    {
+        render.home(req,res);
+    },
+
+    getDataPage: function(req, res)
+    {
+        render.dataPage(req,res);
+    },
+
+    getResultPage: function(req, res)
+    {
+        render.resultPage(req,res);
+    }
 };
 
-function renderFirstPage(req, res, content){
-    res.render('mirath-page1', { 
-        pageHeader:{
-        title: 'Loc8r',
-        strapLine: 'Find places to work with wifi near you!'
-        },
-        sidebar: 'Looking for wifi and a seat?\n'
-        + 'Loc8r helps you find places to work when out and about.'
-        + 'Perhaps with coffee, cake or a pint?'
-        + "Let Loc8r help find the place you're looking for."
-    });
+var render = 
+{
+    home: function(req, res, content)
+    {
+        res.render('mirath-home', { 
+            pageHeader:{
+            title: 'الميراث',
+            strapLine: 'Find places to work with wifi near you!'
+            },
+            sidebar: 'Looking for wifi and a seat?\n'
+            + 'Loc8r helps you find places to work when out and about.'
+            + 'Perhaps with coffee, cake or a pint?'
+            + "Let Loc8r help find the place you're looking for."
+        });
+    },
+
+    dataPage: function(req, res, content)
+    {
+        res.render('mirath-data', { 
+            pageHeader:{
+            title: 'إدخال الورثه',
+            strapLine: 'Find places to work with wifi near you!'
+            },
+            sidebar: 'Looking for wifi and a seat?\n'
+            + 'Loc8r helps you find places to work when out and about.'
+            + 'Perhaps with coffee, cake or a pint?'
+            + "Let Loc8r help find the place you're looking for."
+        });
+    },
+
+    resultPage: function(req, res, content)
+    {
+        res.render('mirath-result', { 
+            pageHeader:{
+            title: 'اﻷنصبه',
+            strapLine: 'Find places to work with wifi near you!'
+            },
+            sidebar: 'Looking for wifi and a seat?\n'
+            + 'Loc8r helps you find places to work when out and about.'
+            + 'Perhaps with coffee, cake or a pint?'
+            + "Let Loc8r help find the place you're looking for."
+        });
+    },
 }
+ 
+
+
+
+
+
+
+
