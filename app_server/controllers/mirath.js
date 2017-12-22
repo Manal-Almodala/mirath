ed4cc46666bca4dc3281a0427d4e2263b8e3e7f3
Yasso96 <yassin.abdohassan@gmail.com>
2017-12-22 19:46:37 +0800

    Remove unnecessary data from rendering res

--- app_server/controllers/mirath.js
+++ app_server/controllers/mirath.js
@@ -34,9 +34,10 @@ var render =
     home: function(req, res, content)
     {
         res.render('mirath-home', { 
-            pageHeader:{
-            title: 'الميراث',
-            strapLine: 'Find places to work with wifi near you!'
+            pageHeader:
+            {
+                title: 'الميراث',
+                strapLine: 'Find places to work with wifi near you!'
             },
             sidebar: 'Looking for wifi and a seat?\n'
             + 'Loc8r helps you find places to work when out and about.'
@@ -47,15 +48,11 @@ var render =
 
     dataPage: function(req, res, content)
     {
-        res.render('mirath-data', { 
-            pageHeader:{
-            title: 'إدخال الورثه',
-            strapLine: 'Find places to work with wifi near you!'
-            },
-            sidebar: 'Looking for wifi and a seat?\n'
-            + 'Loc8r helps you find places to work when out and about.'
-            + 'Perhaps with coffee, cake or a pint?'
-            + "Let Loc8r help find the place you're looking for."
+        res.render('mirath-data', {
+            pageHeader: 
+            {
+                title: 'إدخال الورثه'
+            }
         });
     },
 
