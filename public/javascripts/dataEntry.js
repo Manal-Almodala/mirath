99438f6235a7aa1d56652e5566e9fae056c6d692
Yasso96 <yassin.abdohassan@gmail.com>
2017-12-30 16:59:56 +0800

    Make data entry form dynamic using jquery script
    To dynamically enable and disable the input text field, we listen
    to checkbox change event and modify the field properties
    accordingly
    
    We define a form's submit event handler. This handler will
    implement all the necessary front-end form data validation

--- public/javascripts/dataEntry.js
+++ public/javascripts/dataEntry.js
@@ -1,1 +1,22 @@
-$("#rep").html("hello");
+$("#alwrathaData").submit(function(event) {
+   //alert($("form").serializeArray());
+   //alert("Submitted");
+
+});
+
+$("input[type='checkbox']").change(function() {
+    if(this.checked)
+    {
+        $(this).closest("span").siblings().last().attr("disabled", false);
+    }  
+    else
+    {
+        $(this).closest("span").siblings().last().val(0);
+        $(this).closest("span").siblings().last().attr("disabled", true);  
+    }
+});
+
+
+
+
+
