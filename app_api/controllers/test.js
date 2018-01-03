5b30fe7229ed30dccbb4374dd29003001e92c088
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-03 21:59:17 +0800

    Develop client mirath data validation
    Awratha count must be positive nonzero number. To prevent the user
    from submitting invalid data, we create Alwratha class to store
    the entered form data and validate it

--- app_api/controllers/test.js
+++ app_api/controllers/test.js
@@ -1,7 +1,16 @@
-"use strict";
-function foo() {
-    console.log("Hi there");
+require("../models/alwratha");
+
+
+if(isNaN("1"))
+{
+    console.log("case 1");  
 }
+if(parseInt("R",10) === 'NaN')
+{
+    console.log("case 2");  
+}
+console.log(parseInt("R", 10))
+
 
 
 
--- public/javascripts/dataEntry.js
+++ public/javascripts/dataEntry.js
@@ -1,5 +1,60 @@
+class Alwratha{
+    constructor(data){
+        this.data = data;
+    }
+
+    get data()
+    {
+        return this._data;
+    }
+
+    set data(value)
+    {
+        this._data = value;
+    }
+
+    get dataIsValid()
+    {
+        this.dataIsValid = (this.isNumericValues() && this.isValuesGreaterThanZero());
+        return this._dataIsValid;
+    }
+
+    set dataIsValid(value)
+    {
+        this._dataIsValid = value;
+    }
+
+    isNumericValues()
+    {
+        var isValid = true;
+        this.data.forEach(warith => {
+            if(isNaN(warith.value))
+            {
+                isValid = false;
+            }  
+        }); 
+        return isValid;
+    }
+
+    isValuesGreaterThanZero()
+    {
+        var isValid = true;
+        this.data.forEach(warith => {
+            if(warith.value <= 0)
+            {
+                isValid = false;
+            }  
+        }); 
+        return isValid;
+    }
+}
+
 $("#alwrathaData").submit(function(event) {
-   event.preventDefault();
+    var  alwratha = new Alwratha($("#alwrathaData").serializeArray());
+    if(!alwratha.dataIsValid)
+    {
+        event.preventDefault();
+    }
 });
 
 $("input[type='checkbox']").change(function() {
