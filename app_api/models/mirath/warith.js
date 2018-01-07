e638789db55b36c009e2dedf793f5137900834e4
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-07 23:46:28 +0800

    Add and define warith class

---
+++ app_api/models/mirath/warith.js
@@ -0,0 +1,11 @@
+class Warith
+{
+    constructor(){
+        this.count = 0;
+        this.fortune = {
+            money: 0,
+            property: 0
+        }
+    }
+}
+module.exports = Warith;
