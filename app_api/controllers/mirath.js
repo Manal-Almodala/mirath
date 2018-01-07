785a9ab2a91dae23971148fb0a645726b8dea372
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-07 14:48:24 +0800

    Develop mirath api controllers

--- app_api/controllers/mirath.js
+++ app_api/controllers/mirath.js
@@ -1,5 +1,5 @@
 const helperModule = require('./shared-functions');
-const Alwratha = require("../models/alwratha");
+const Alwratha = require("../models/mirath/alwratha");
 
 var alwratha = new Alwratha();
 
@@ -7,13 +7,39 @@ module.exports = {
     readAlwrathaList: function(req, res){
         var alwrathaList = alwratha.getList();
         if(alwrathaList)
-        helperModule.sendJsonResponse(res, 200, alwrathaList);
+        {
+            helperModule.sendJsonResponse(res, 200, alwrathaList);
+        }
+        else
+        {
+            helperModule.sendJsonResponse(res, 500, "No data was found");
+        }     
     },
+
     addAlwrathaData: function(req, res){
         alwratha.data = req.body;
         helperModule.sendJsonResponse(res, 201, alwratha.data);
     },
-    calculateMirath: function(req, res){
 
+    calculateMirath: function(req, res){
+        alwratha.result = {
+            "ابن":
+            { 
+                count:"3",
+                fortune: "النصف"
+            },
+            "أب":
+            {
+                count:"1",
+                fortune: "الربع"
+            },
+            "بنت":
+            {
+                count:"3",
+                fortune: "الربع"
+            }
+        };
+        
+        helperModule.sendJsonResponse(res, 200, alwratha.result);
     },
 };
