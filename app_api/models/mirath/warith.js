2b2b804338c267a0fc6b108e74a4de3adda5a732
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-10 22:35:54 +0800

    Define Fortune class
    Following a refactoring step and to improve code reusability, we
    decided to create a class whose instances stores the fortune of
    each warith in Alwratha class

--- app_api/models/mirath/warith.js
+++ app_api/models/mirath/warith.js
@@ -2,10 +2,20 @@ class Warith
 {
     constructor(){
         this.count = 0;
+        this.fortune = new Fortune();
+    }
+}
+
+class Fortune
+{
+    constructor()
+    {
         this.fortune = {
+            ratio: 0.0,
             money: 0,
             property: 0
         }
     }
 }
-module.exports = Warith;
+module.exports.warith = Warith;
+module.exports.fortune = Fortune;
