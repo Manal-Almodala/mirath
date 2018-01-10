a80b0b5101ef8171a7743ad06e380fdfcc60f349
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-10 23:25:43 +0800

    Change Tarika properties initiation
    Previously, tarika.money and tarika.property would be initiated
    in the class constructor which takes tarika object as parameter.
    The tarika object parameter actually comes from a form input data.
    
    To avoid any form data preprocessing and consequently ease MVC
    integration,we decided to individually assign each form input to
    its respective Tarika property. We accomplish this by adding
    property setter and getter for each Tarika propery

--- app_api/models/mirath/tarika.js
+++ app_api/models/mirath/tarika.js
@@ -2,19 +2,39 @@ const alwratha = require("../../controllers/mirath").model;
 
 class Tarika
 {
-    constructor(tarika)
+    constructor()
     {
-        this.money = tarika.money;
-        this.property = tarika.property;
+        this.money = 0;
+        this.property = 0;
         this._remainder = {
             money: 0,
             property: 0,
         }
     }
 
+    get money()
+    {
+        return this._money;
+    }
+
+    set money(value)
+    {
+        this._money = value;
+    }
+
+    get property()
+    {
+        return this._property;
+    }
+
+    set property(value)
+    {
+        this._property = value;
+    }
+
     get remainder()
     {
-        let consumedMoney = 0, consumedProperty = 0,;
+        let consumedMoney = 0, consumedProperty = 0;
         for(var warith in alwratha.data)
         {
             consumedMoney += warith.count * warith.fortune.money;
