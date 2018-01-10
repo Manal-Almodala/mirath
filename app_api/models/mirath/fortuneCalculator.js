85aaeb70f97eb9e9ac9b49ee382a857faaf28c41
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-10 22:45:53 +0800

    Rename rules.js to fortuneCalculator.js

---
+++ app_api/models/mirath/fortuneCalculator.js
@@ -0,0 +1,59 @@
+const alwratha = require("../../controllers/mirath").model;
+const Fortune = require("./warith").fortune;
+
+module.exports = 
+{
+    get "ابن"(){
+        var fortune = new Fortune();
+        return fortune;
+    },
+
+    get "ابن ابن"(){
+        var fortune = new Fortune();
+        return fortune ;
+    },
+
+    get "زوج"(){
+        var fortune = new Fortune();
+        fotune.ratio = 0.5;
+
+        if(alwratha.hasDirectChild || alwratha.hasSonsChild)
+        {
+            fotune.ratio = 0.25;
+        }
+
+        return fortune;
+    },
+
+    get "زوجه"(){
+        var fortune = new Fortune();
+        fotune.ratio = 0.25;
+
+        if(alwratha.hasDirectChild || alwratha.hasSonsChild)
+        {
+            fotune.ratio = 0.125;
+        }
+
+        fotune.ratio /= alwratha.data["زوجه"].count;
+        return fortune  
+    },
+
+    get "أم"(){
+        var fortune = new Fortune();
+        fotune.ratio = 0.333;
+
+        if(alwratha.hasBrothersOrSisters || alwratha.hasDirectChild 
+            || alwratha.hasSonsChild)
+        {
+            fotune.ratio = 0.167;
+        }
+        
+        /* Add two more cases 
+         * 1. 1/3 + the remainder 
+         * 2. 1/3 of the remainder 
+        */
+
+        return fortune;
+    },
+};
+
