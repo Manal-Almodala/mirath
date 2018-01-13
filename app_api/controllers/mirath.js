f63f38f894381bf1e9e7f295c27eda6586057aa4
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-13 18:19:40 +0800

    Define remainder fortune calculation logic

--- app_api/controllers/mirath.js
+++ app_api/controllers/mirath.js
@@ -1,8 +1,9 @@
 const helperModule = require('./shared-functions');
-const Alwratha = require("../models/mirath/alwratha");
-const Tarika = require("../models/mirath/tarika");
 
+const Alwratha = require("../models/mirath/alwratha");
 var alwratha = new Alwratha();
+
+const Tarika = require("../models/mirath/tarika");
 var altarika = new Tarika();
 
 module.exports.alwratha = alwratha;
@@ -34,11 +35,41 @@ module.exports.controllers = {
     {
         for (var warith in alwratha.data) 
         {
-            alwratha.data[warith].fortune = fortuneCalculator[warith];
+            alwratha.data[warith].fortune.ratio = fortuneCalculator[warith];
+            alwratha.data[warith].fortune.calculate(altarika);
         }
-        
+
+        calculateTarikaRemainder();
+
+        for(var warith in alwratha.data)
+        {
+            if(alwratha.data[warith].fortune.hasRemainder)
+            {
+                alwratha.data[warith].fortune.ratio = fortuneCalculator[warith];
+                alwratha.data[warith].fortune.addRemainderWorth(altarika);
+            }
+        }
+
         helperModule.sendJsonResponse(res, 200, alwratha.data);
     },
 };
+
+function calculateTarikaRemainder()
+{
+    var consumed = {
+        money: 0,
+        property: 0
+    };
+
+    for(var warith in alwratha.data)
+    {
+        let count = alwratha.data[warith].count;
+        consumed.money += count * alwratha.data[warith].fortune.money;
+        consumed.property += count * alwratha.data[warith].fortune.property;
+    }
+
+    altarika.remainder.money = altarika.money - consumed.money;
+    altarika.remainder.property = altarika.property - consumed.property;
+}
 
 
