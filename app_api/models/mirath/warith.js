9d280b147083ed42ac6b72ff823c4462d41da959
Yasso96 <yassin.abdohassan@gmail.com>
2018-01-13 17:50:33 +0800

    Add hasRemainder & addRemainderWorth() to Writh class

--- app_api/models/mirath/warith.js
+++ app_api/models/mirath/warith.js
@@ -5,12 +5,19 @@ class Fortune
         this.ratio = 0;
         this.money = 0;
         this.property = 0;
+        this.hasRemainder = false;
     }
 
     calculate(tarika)
     {
         this.money = this.ratio * tarika.money;
         this.property = this.ratio * tarika.property;
+    }
+
+    addRemainderWorth(tarika)
+    {
+        this.money += this.ratio * tarika.remainder.money;
+        this.property += this.ratio * tarika.remainder.property;
     }
 }
 module.exports.fortune = Fortune;
