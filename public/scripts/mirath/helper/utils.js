635132860e60de254cdf271fd0f58ef41db939ac
Yasso96 <yassin.abdohassan@gmail.com>
2018-03-28 00:36:38 +0800

    Create utils to contain general helper methods

---
+++ public/scripts/mirath/helper/utils.js
@@ -0,0 +1,34 @@
+var crossPageUtilities = {
+    enableTooltips: function(){
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    },
+
+    clearPlaceholder: function(selector, event){
+        $(selector).on(event, function() {
+            $(this).attr("placeholder", " ");
+        });
+    }, 
+    
+    toggleInputOnCheckboxChange: function(){
+        $("input[type='checkbox']").change(function() {
+            if(this.checked)
+            {
+                $(this).closest("span").siblings().last().attr("disabled", false);
+            }  
+            else
+            {
+                $(this).closest("span").siblings().last().val(0);
+                $(this).closest("span").siblings().last().attr("disabled", true);  
+            }
+        });
+    }
+};
+module.exports = crossPageUtilities;
+
+
+
+
+
+
